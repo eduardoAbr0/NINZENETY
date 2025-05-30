b746147af66447abdc17057551b89d0d84088542
Eduardo Alejo <eduardo.br@tecjerez.edu.mx>
2025-05-30 00:22:23 -0600

    Pagina preguntas escaneo

--- app/src/main/java/com/tecjerez/innovtc/EscActivity.java
+++ app/src/main/java/com/tecjerez/innovtc/EscActivity.java
@@ -1,4 +1,36 @@
 package com.tecjerez.innovtc;
 
-public class EscActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EscActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.escaneo);
+
+        ImageButton homeBtn = findViewById(R.id.navHome);
+        ImageButton vidBtn = findViewById(R.id.navVid);
+        ImageButton escBtn = findViewById(R.id.navEsc);
+
+        homeBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(EscActivity.this, HomeActivity.class);
+            startActivity(intent);
+        });
+
+        vidBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(EscActivity.this, VidActivity.class);
+            startActivity(intent);
+        });
+
+        escBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(EscActivity.this, EscActivity.class);
+            startActivity(intent);
+        });
+
+    }
 }
